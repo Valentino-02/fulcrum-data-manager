06461d0c110d751b3c5501a126e92002483da328
Valentino-02 <collazo.valentino@gmail.com>
2024-09-09 10:16:31 -0300

    add table component

--- actions/setActions.ts
+++ actions/setActions.ts
@@ -1,13 +1,18 @@
-import { encodedRedirect } from "@/utils/utils";
 import { createClient } from "@/utils/supabase/server";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 export async function getSets() {
   const supabase = createClient();
-  let { data, error } = await supabase.from("sets").select("*");
-  if (error) return []
-  console.log(data)
+  let { data, error } = await supabase
+    .from("sets")
+    .select(`
+      *,
+      aspects (*),
+      tags (*)
+    `)
+  if (error) {
+    console.error("Error fetching sets with aspects:", error);
+    return [];
+  }
   return data;
 }
 
--- app/(main)/sets/page.tsx
+++ app/(main)/sets/page.tsx
@@ -1,3 +1,13 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 import { getSets } from "@/actions/setActions";
 import Link from "next/link";
 
@@ -5,7 +15,28 @@ export default async function SetsPage() {
   const sets = await getSets()
   return (
     <div className="flex-1 flex flex-col gap-6 px-4">
-    {sets?.map((set) => <div>{set.name}</div>)}
+    <Table>
+  <TableCaption>A list of Aspect Sets.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Name</TableHead>
+      <TableHead>Aspects</TableHead>
+      <TableHead>Related Tags</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {sets?.map((set) => <TableRow key={set.id}>
+   <TableCell className="font-medium">{set.name}</TableCell>
+   <TableCell>
+   {set.aspects.map((aspect: { name: any; }) => aspect.name).join(", ")}
+   </TableCell>
+   <TableCell>
+   {set.tags.map((tag: { name: any; }) => tag.name).join(", ")}
+   </TableCell>
+ </TableRow>)}
+ 
+  </TableBody>
+</Table>
     </div>
   );
 }
