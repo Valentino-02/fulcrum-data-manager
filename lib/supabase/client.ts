bedab8cf3df5d00df2945d9b91c4e7d451c09355
Valentino-02 <collazo.valentino@gmail.com>
2024-09-24 09:27:51 -0300

    rollback TS DB types

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,8 @@
-import { Database } from "@/types/db";
+
 import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
-  createBrowserClient<Database>(
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
